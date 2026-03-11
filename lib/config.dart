896c1295e6167dd05c8a911b88a18cce93352423
SeungHyeon <shlee08509@gmail.com>
2026-03-11 16:32:32 +0900

    feat: inbound scan + google sheet integration

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 // lib/config.dart
 class AppConfig {
   static const String webAppUrl =
-      'https://script.google.com/macros/s/AKfycbxzsDXV51FjA94tN8SKG_lLL8uSATR5qO_VpAaBEnINNFslFFOFr710a9bHnDh0vTc/exec';
+      'https://script.google.com/macros/s/AKfycbxpb23Z-jm98gxuoMeV4n9fUG3l4VObFKW27j3LS-DXrlW8ZwJI3DT0UxjaIcgpQ2mgxQ/exec';
 }
